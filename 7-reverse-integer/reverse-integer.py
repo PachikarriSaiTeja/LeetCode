a3e68a84df9d81e0bdcdb784a3c3fbdceac8e5af
PachikarriSaiTeja <121477898+PachikarriSaiTeja@users.noreply.github.com>
2024-06-13 15:15:24 +0530

    Time: 11 ms (87.40%) | Memory: 11.6 MB (31.44%) - LeetSync

--- 7-reverse-integer/reverse-integer.py
+++ 7-reverse-integer/reverse-integer.py
@@ -1,14 +1,26 @@
 class Solution(object):
     def reverse(self, x):
-        h=2**31-1
-        l=-2**31-1
-        ab=abs(x)
-        y=int(str(ab)[::-1])
-        if(y>h or y<l):
+        if x==0:
+            return 0
+        r=(str(x)[::-1])
+        l=[]
+        hi=2**31-1
+        lo=-2**31-1
+        for i in r:
+            l.append(i)
+        for j in range(len(l)):
+            if l[0]=='0' and l[-1]=='-':
+                l.remove('0')
+                l.remove('-')
+            elif l[0]=='0':
+                l.remove('0')
+            elif l[-1]=='-':
+                l.remove('-')
+        rev=int(''.join(l))
+        if(rev>hi or rev<lo):
             return 0
         elif(x>0):
-            return y
-        elif(x<0):
-            return -y
-        return 0
-        
+            return rev
+        elif(x<0):  
+            return -rev
+
