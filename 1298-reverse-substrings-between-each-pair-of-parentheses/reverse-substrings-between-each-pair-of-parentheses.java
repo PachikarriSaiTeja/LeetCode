ca0ee2dc40e646679d4a43fe23f0b6e68d567489
PachikarriSaiTeja <121477898+PachikarriSaiTeja@users.noreply.github.com>
2024-07-11 11:10:52 +0530

    Time: 6 ms (28.25%) | Memory: 42 MB (46.58%) - LeetSync

--- 1298-reverse-substrings-between-each-pair-of-parentheses/reverse-substrings-between-each-pair-of-parentheses.java
+++ 1298-reverse-substrings-between-each-pair-of-parentheses/reverse-substrings-between-each-pair-of-parentheses.java
@@ -1,37 +1,37 @@
-import java.util.Stack;
-
 class Solution {
     public String reverseParentheses(String s) {
-        StringBuilder sb = new StringBuilder();
-        Stack<Character> st = new Stack<>();
-        
-        for (int i = 0; i < s.length(); i++) {
-            char o = s.charAt(i);
-            
-            if (o == ')') {
-                while (st.peek() != '(') {
+        StringBuilder sb=new StringBuilder();
+        Stack<Character> st=new Stack<>();
+        int c=0;
+        for(int i=0;i<s.length();i++)
+        {
+            Character o=s.charAt(i);
+            if(o.equals(')'))
+            {
+                while(!st.peek().equals('('))
+                {
                     sb.append(st.pop());
+                    c++;
+                    
                 }
-                st.pop(); // Pop the '('
-                
-                // Push reversed substring back into stack
-                for (int j = 0; j < sb.length(); j++) {
+                st.pop();
+                for(int j=0;j<sb.length();j++)
+                {
                     st.push(sb.charAt(j));
                 }
-                
-                // Clear StringBuilder after insertion into stack
                 sb.setLength(0);
-            } else {
+                
+            }
+            else{
                 st.push(o);
             }
         }
         
-        // Build the result string from stack
-        StringBuilder result = new StringBuilder();
-        while (!st.isEmpty()) {
-            result.insert(0, st.pop());
+        while(!st.isEmpty())
+        {
+           sb.append(st.pop());
         }
+        return sb.reverse().toString();
         
-        return result.toString();
     }
 }
