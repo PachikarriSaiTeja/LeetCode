cff361c807f8cef0b580f9220316ede36e30082c
PachikarriSaiTeja <121477898+PachikarriSaiTeja@users.noreply.github.com>
2025-03-06 10:18:47 +0530

    Time: 125 ms (5.02%) | Memory: 45.4 MB (90.54%) - LeetSync

---
+++ 3227-find-missing-and-repeated-values/find-missing-and-repeated-values.java
@@ -0,0 +1,43 @@
+class Solution {
+    public int[] findMissingAndRepeatedValues(int[][] grid) {
+
+        int m=grid.length;
+        int n=grid[0].length;
+        int val=m*n;
+        int[] arr=new int[val+1];
+        int[] ans=new int[2];
+        int o=0;
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<m;j++)
+            {
+                arr[grid[i][j]]++;
+            }
+        }
+        for(int i:arr)
+        {
+            System.out.println(i);
+        }
+        for(int i=0;i<=val;i++){
+
+                if(arr[i]==0 && i!=0)
+                {
+                    ans[1]=i;
+                    o+=1;
+                    
+                }
+                if(arr[i]==2 && i!=0)
+                {
+                    ans[0]=i;
+                    o+=1;
+
+                }
+            if(o==2)
+            {
+                return ans;
+            }
+        }
+
+         return ans;
+    }
+}
