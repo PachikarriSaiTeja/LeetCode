2a2448842558b4267cafd7c6ee5c8294e3d1dfbe
PachikarriSaiTeja <121477898+PachikarriSaiTeja@users.noreply.github.com>
2025-08-30 22:53:07 +0530

    Time: 0 ms (100.00%) | Memory: 42.4 MB (25.50%) - LeetSync

--- 48-rotate-image/rotate-image.java
+++ 48-rotate-image/rotate-image.java
@@ -1,77 +1,34 @@
 class Solution {
     public void rotate(int[][] matrix) {
         
-       int r=matrix.length;
-       int[][] arr=new int[r][r];
-    
+        for(int i=0 ; i<matrix.length;i++)
+        {
+            for(int j=i ; j<matrix.length;j++)
+            {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+
+            }
+        }
+    for(int i=0 ; i< matrix.length ;i++)
+    {
+        int f=0;
+    int l=matrix.length-1;
+        while(f<=l)
+        {
+           int temp =  matrix[i][f];
+           matrix[i][f] = matrix[i][l];
+           matrix[i][l] = temp;
+           f++;
+           l--;
+
+        }
         
-for(int i=0;i<r;i++)
-{
-for(int j=0;j<r;j++)
-{
-    arr[i][j]=matrix[i][j];
-
-}
-}
-// System.out.printl
-
-
-for(int i=0;i<r;i++)
-{
-for(int j=0;j<r;j++)
-{
-	matrix[i][j]=arr[j][r-i-1];
-
-
-}
-}        
-for(int i=0;i<r;i++)
-{
-for(int j=0;j<r;j++)
-{
-    arr[i][j]=matrix[i][j];
-
-}
-}
-// System.out.printl
-
-
-for(int i=0;i<r;i++)
-{
-for(int j=0;j<r;j++)
-{
-	matrix[i][j]=arr[j][r-i-1];
-
-
-}
-}        
-for(int i=0;i<r;i++)
-{
-for(int j=0;j<r;j++)
-{
-    arr[i][j]=matrix[i][j];
-
-}
-}
-// System.out.printl
-
-
-for(int i=0;i<r;i++)
-{
-for(int j=0;j<r;j++)
-{
-	matrix[i][j]=arr[j][r-i-1];
-
-
-}
-}
-
-
     }
-}
+        
+    }
+    }
 
-        // 00,01,02,03     03,1,3,23,33
-        // 10,11,12,13     02,12,22,32
-        // 20,21,22,23     01,11,21,31
-        // 30,31,32,33     00,10,20,30
-         
+    
+   
